ea1f7473fabba67f82b43c74356e06aceda8929a
= <=>
2024-05-05 21:20:25 +0000

    Emptied Index page

--- public/index.php
+++ public/index.php
@@ -1,12 +1,2 @@
 <?php
 require(__DIR__."/../vendor/autoload.php");
-use App\System\Core;
-use Lazarus\LazarusDb\Database;
-Core::Boot();
-
-// Added to prevent Permission Error Sesion directory if needed.
-Core::Map("sessions","/var/www/sessions",true);
-session_save_path(sessions);
-// Start Session
-session_start();
-$database = new Database();
