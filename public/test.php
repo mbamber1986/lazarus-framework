40af24eb095dda4f22670e6f267d692950582909
mbamber1986 <mbamber1986@gmail.com>
2024-05-16 00:12:51 +0100

    Updated FIles to support DateTime

--- public/test.php
+++ public/test.php
@@ -2,10 +2,11 @@
 require(__DIR__ . "/../vendor/autoload.php");
 use App\System\Core;
 use App\System\StaticClasses\Date;
+use LazarusPhp\AuthManager\Auth;
 use lazarusphp\lazarusdb\Database;
 Core::Boot();
-$date = Date::AddDate("now","London");;
-$date2 = Date::AddDate("22-03-1972","London");
-echo Date::Informat($date,"d/m/y H:i:s a");
-echo "<br> Years since : ";
-echo Date::GetDifference($date,$date2,"%Y Years %M Months and %D Days ");
+
+Date::PushTimeZone("Germany","Germany/Berlin");
+echo Date::PullTimezone("Germany");
+echo "<br>";
+echo Date::AddDate("now")->format("D/M/Y");
