0e2319739bf5962cba9b4700df3788e718d4cfdc
mbamber1986 <mbamber1986@gmail.com>
2024-06-09 14:45:19 +0100

    Added Post and Get Requests Features

--- public/test.php
+++ public/test.php
@@ -1,11 +1,21 @@
 <?php
+use App\System\Classes\Required\Requests;
 require(__DIR__ . "/../vendor/autoload.php");
 
-use App\System\Classes\Required\Date;
+
 use App\System\Core;
-use App\System\Classes\Required\VersionControl;
-use LazarusPhp\DatabaseManager\Database;
-use LazarusPhp\SessionManager\Sessions;
-use App\System\Classes\Required\Csrf;
 
 Core::Boot();
+$request = new Requests();
+if($request->OnGet("username") && $request->OnGet("email"))
+{
+    echo $request->request("username");
+    echo $request->request("email");
+}
+
+
+?>
+<form method="post">
+<input type="text" name="username" value="">
+<button name="save">Save</button>
+</form>
