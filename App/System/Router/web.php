2b4ff7dca98787ba7c31ccc67e41613ca4454d57
martin bamber <mbamber1986@gmail.com>
2024-06-19 23:12:35 +0100

    Created Base Router File

--- App/System/Router/web.php
+++ App/System/Router/web.php
@@ -1,3 +1,20 @@
 <?php
-// Web Routing Script.
-?>
+use MiladRahimi\PhpRouter\Router;
+use MiladRahimi\PhpRouter\Exceptions\RouteNotFoundException;
+use Laminas\Diactoros\Response\HtmlResponse;
+
+$router = Router::create();
+
+$router->get('/', function () {
+    return 'Welcome to LazarusPhp Framework';
+});
+
+try {
+    $router->dispatch();
+} catch (RouteNotFoundException $e) {
+    // It's 404!
+    $router->getPublisher()->publish(new HtmlResponse('Not found.', 404));
+} catch (Throwable $e) {
+    // Log and report...
+    $router->getPublisher()->publish(new HtmlResponse('Internal error.', 500));
+}
