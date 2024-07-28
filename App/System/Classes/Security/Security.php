16041ae3a4e8e3cef95846663d29c10cbfe8be6e
mbamber1986 <mbamber1986@gmail.com>
2024-07-28 23:27:52 +0100

    Renamed Namespaces to reflect New Changes

--- App/System/Classes/Security/Security.php
+++ App/System/Classes/Security/Security.php
@@ -1,8 +1,31 @@
 <?php
 namespace App\System\Classes\Security;
+use App\System\Classes\Requests\Requests;
+use App\System\Classes\Validation\Validation;
 
 class Security
 {
+
+    private $request;
+    public function __construct()
+    {
     
+    }
+
+    public function hash($password,$encryption=PASSWORD_DEFAULT)
+    {
+        return password_hash($password,$encryption);
+    }
+
+    public function VerifyHash($local,$remote)
+    {
+        if(password_verify($local,$remote))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
-?>
