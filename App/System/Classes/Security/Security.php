d2e1183b312507bff78e1fce440dbea9360ac915
mbamber1986 <mbamber1986@gmail.com>
2024-07-29 22:35:24 +0100

    Added hasstromngpassword to Security

--- App/System/Classes/Security/Security.php
+++ App/System/Classes/Security/Security.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\System\Classes\Security;
+
 use App\System\Classes\Validation\Validation;
 
 class Security extends Validation
@@ -31,13 +33,98 @@ class Security extends Validation
         echo '<input type="text" name="csrf_token" value="' . $this->GetToken() . '">';
     }
 
-    public function hash($password,$encryption=PASSWORD_DEFAULT)
+    public function hash($password, $encryption = PASSWORD_DEFAULT)
     {
-        return password_hash($password,$encryption);
+        return password_hash($password, $encryption);
     }
 
-    public function VerifyHash($local,$remote)
+    public function VerifyHash($local, $remote)
     {
-        return (password_verify($local,$remote) == true) ? true : false;
+        return (password_verify($local, $remote) == true) ? true : false;
+    }
+
+    public function hasStrongPassword($password, $options)
+    {
+        // Options Support upper lower numbers specials minlength maxlength
+
+        $this->pwCheck = true;
+        // Check if Uppercase
+        $uppercase = preg_match('@[A-Z]@', $password);
+        // Check if Lower case
+        $lowercase = preg_match('@[a-z]@', $password);
+        // Check for numbers
+        $number = preg_match('@[0-9]@', $password);
+        // Check for String length
+        $length = strlen($password);
+        $specialChars = preg_match('@[^\w]@', $password);
+
+        $explode = explode("|", $options);
+
+        foreach ($explode as $exploded) {
+            $this->passwordValidate[$exploded] = true;
+        }
+
+        $validPw = (object) $this->passwordValidate;
+
+        if (isset($validPw->uppercase)) {
+            if ($validPw->uppercase == true) {
+                if (!$uppercase) {
+                    $this->pwCheck = false;
+                    $this->errors[] = "Password Must contaoin at least one Uppercase Letter";
+                }
+            }
+        }
+
+        if (isset($validPw->lowercase)) {
+            if ($validPw->lowercase == true) {
+                if (!$lowercase) {
+                    $this->pwCheck = false;
+                    $this->errors[] = "Password Must have at least one lowercase letter";
+                }
+            }
+        }
+
+        if (isset($validPw->number)) {
+            if ($validPw->number == true) {
+                if (!$number) {
+                    $this->pwCheck = false;
+                    $this->errors[] = "Password must contain at least one number";
+                }
+            }
+        }
+
+        if (isset($validPw->min)) {
+            if ($validPw->min == true) {
+                if ($length < 8) {
+                    $this->pwCheck = false;
+                    $this->errors[] = "Password Must be a minimum of 8 Characters";
+                }
+            }
+        }
+
+        if (isset($validPw->max)) {
+            if ($validPw->max == true) {
+                if ($length > 20) {
+                    $this->pwCheck = false;
+                    $this->errors[] = "Password cannit exceed 20 characters";
+                }
+            }
+        }
+
+        if (isset($validPw->specials)) {
+            if ($validPw->specials == true) {
+                if (!$specialChars) {
+                    $this->pwCheck = false;
+                    $this->errors[] = "Password Must contain at least one Special Character";
+                }
+            }
+        }
+
+
+        if ($this->pwCheck == false) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
